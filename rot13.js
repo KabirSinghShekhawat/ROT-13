a029392a083f77ec48a4decfb2b466c0434b95fd
Kabir Singh Shekhawat <kabirsinghshekhawat1789@gmail.com>
2021-06-19 15:54:43 +0530

    Refactor code, remove redundant else, else-if

--- rot13.js
+++ rot13.js
@@ -1,70 +1,68 @@
-const inputField = document.querySelector('#passwordField');
-const outputField = document.querySelector('#passwordOutput');
+const inputField = document.querySelector('#passwordField')
+const outputField = document.querySelector('#passwordOutput')
 
-const rot13RotationValue = 13;
-const lowerCaseLowerLimit = 97, lowerCaseUpperLimit = 122,                  
-      upperCaseLowerLimit = 65, upperCaseUpperLimit = 90;
+const rot13RotationValue = 13
+const
+    lowerCaseLowerLimit = 97,
+    lowerCaseUpperLimit = 122,
+    upperCaseLowerLimit = 65,
+    upperCaseUpperLimit = 90;
 
-function rotateAlphabet(character, characterCase){
-    let characterCode = character.charCodeAt(0);
-    if(characterCase === 'lowerCase'){
-        characterCode += rot13RotationValue;
-        if(characterCode > lowerCaseUpperLimit){
-            characterCode -= lowerCaseUpperLimit;
-            characterCode += lowerCaseLowerLimit - 1;
-        }
-    }else if(characterCase === 'upperCase'){
-        characterCode += rot13RotationValue;
-        if(characterCode > upperCaseUpperLimit){
-            characterCode -= upperCaseUpperLimit 
-            characterCode += upperCaseLowerLimit - 1;
-        }
-    }else{
-        return character;
+function rotate(characterCode, lowerLimit, upperLimit) {
+    characterCode += rot13RotationValue
+    if (characterCode > upperLimit) {
+        characterCode -= upperLimit
+        characterCode += lowerLimit - 1
     }
-    return String.fromCharCode(characterCode);
+    return String.fromCharCode(characterCode)
 }
 
-function isUppercaseAlphabet(characterValue){
+function rotateAlphabet(character, characterCase) {
+    const characterCode = character.charCodeAt(0)
+    if (characterCase === 'lowerCase')
+        return rotate(characterCode, lowerCaseLowerLimit, lowerCaseUpperLimit)
+    if (characterCase === 'upperCase')
+        return rotate(characterCode, upperCaseLowerLimit, upperCaseUpperLimit)
+    return character
+}
+
+function isUppercaseAlphabet(characterValue) {
     return characterValue >= upperCaseLowerLimit &&
-           characterValue <= upperCaseUpperLimit;
+        characterValue <= upperCaseUpperLimit;
 }
 
-function isLowercaseAlphabet(characterValue){
+function isLowercaseAlphabet(characterValue) {
     return characterValue >= lowerCaseLowerLimit &&
-           characterValue <= lowerCaseUpperLimit;
+        characterValue <= lowerCaseUpperLimit
 }
+
 function isAlphabet(character) {
-    let characterValue = character.charCodeAt(0);
-    if(isLowercaseAlphabet(characterValue)){
-        return 'lowerCase';
-    }else if(isUppercaseAlphabet(characterValue)){
-        return 'upperCase';
-    }else{
-        return 'notAlphabet';
-    }
+    let characterValue = character.charCodeAt(0)
+    if (isLowercaseAlphabet(characterValue)) return 'lowerCase'
+    if (isUppercaseAlphabet(characterValue)) return 'upperCase'
+    return 'notAlphabet'
 }
 
 function rot13Alphabet(character) {
-    return rotateAlphabet(character, isAlphabet(character));
+    return rotateAlphabet(character, isAlphabet(character))
 }
 
-function rot13(textInput){
-    if(textInput == null) return;
+function rot13(textInput) {
+    if (!textInput) return
 
-    let finalResult = '';
-    for(let character of String(textInput)){
-        finalResult += rot13Alphabet(character);
-    }
-    return finalResult;
-}   
+    const text = String(textInput)
+    let finalResult = ''
+
+    for (let character of text)
+        finalResult += rot13Alphabet(character)
+    return finalResult
+}
 
 inputField.addEventListener('input', () => {
-    if(!inputField.value){
-        outputField.classList.add('remove');
-    }else{
-        outputField.classList.remove('remove');
-        outputField.textContent = `Encrypted Password : 
-                                   ${rot13(inputField.value)}`;
-    }
-});
+    if (!inputField.value)
+        return outputField.classList.add('remove')
+
+    outputField.classList.remove('remove')
+    outputField.textContent =
+        `Encrypted Password : ${rot13(inputField.value)}`
+})
